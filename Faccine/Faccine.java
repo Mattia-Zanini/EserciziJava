5c17c240b4d81ecda0d18d1d1cad848e71bd6125
Mattia-Zanini <mattzanini2@gmail.com>
2023-10-24 15:19:34 +0200

    .

---
+++ Faccine/Faccine.java
@@ -0,0 +1,9 @@
+public class Faccine{
+	public static void main(String[] args){
+		System.out.println(" ///|||///    ###|||###    %%%%%%");
+		System.out.println("(| O   O |)   (| O   O |) ((~~~~~~)) ");
+		System.out.println(" (   V   )     |   8   |   ))^  ^(( ");
+		System.out.println("(  < _ >  )   (  VwwwV  ) ((  ..  ))");
+		System.out.println(" <_______>     <_______>   )) -- ((");
+	}
+}
